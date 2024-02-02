4404e1268c37f786d7141024fbfa744d6e86bc4c
IanPedroBorges <ianhudback@gmail.com>
2024-02-01 22:45:00 -0300

    update

--- src/components/CardProject/index.tsx
+++ src/components/CardProject/index.tsx
@@ -15,11 +15,11 @@ const initialProject = {
 
 export default function CardProject() {
   const [project, setProject] = useState(initialProject);
-
+  const [projectFilter, setProjectFilter] = useState(ProjectArray)
   const handleClickLess = () => {
     if (project.project1 === 0) {
       setProject({
-        project1: ProjectArray.length - 1,
+        project1: projectFilter.length - 1,
         project2: project.project2 - 1,
         project3: project.project3 - 1,
       });
@@ -27,7 +27,7 @@ export default function CardProject() {
     } else if (project.project2 === 0) {
       setProject({
         project1: project.project1 - 1,
-        project2: ProjectArray.length - 1,
+        project2: projectFilter.length - 1,
         project3: project.project3 - 1,
       });
       return;
@@ -35,7 +35,7 @@ export default function CardProject() {
       setProject({
         project1: project.project1 - 1,
         project2: project.project2 - 1,
-        project3: ProjectArray.length - 1,
+        project3: projectFilter.length - 1,
       });
       return;
     }
@@ -47,21 +47,21 @@ export default function CardProject() {
   };
 
   const handleClickMore = () => {
-    if (project.project1 === ProjectArray.length - 1) {
+    if (project.project1 === projectFilter.length - 1) {
       setProject({
         project1: 0,
         project2: project.project2 + 1,
         project3: project.project3 + 1,
       });
       return;
-    } else if (project.project2 === ProjectArray.length - 1) {
+    } else if (project.project2 === projectFilter.length - 1) {
       setProject({
         project1: project.project1 + 1,
         project2: 0,
         project3: project.project3 + 1,
       });
       return;
-    } else if (project.project3 === ProjectArray.length - 1) {
+    } else if (project.project3 === projectFilter.length - 1) {
       setProject({
         project1: project.project1 + 1,
         project2: project.project2 + 1,
@@ -76,61 +76,105 @@ export default function CardProject() {
     });
   };
 
+  const handleClickFilter = (filter: string) => {
+    if(filter === "All"){
+      console.log('ntrou')
+      setProjectFilter(ProjectArray)
+      setProject(initialProject)
+      return
+    }
+
+    if(filter === "Destaques") {
+      console.log('ntrou')
+      setProjectFilter(ProjectArray.filter((project) => project.highlights))
+      setProject(initialProject)
+      return
+    }
+
+    if(filter === 'Front-End') {
+      console.log('ntrou')
+      setProjectFilter(ProjectArray.filter((project) => project.frontend))
+      setProject(initialProject)
+      return
+    }
+    if(filter === 'Back-End') {
+      console.log('ntrou')
+      setProjectFilter(ProjectArray.filter((project) => project.backend))
+      setProject(initialProject)
+      return
+    }
+  }
+
   return (
+    <div>
+      <ul className={styles.MainNavigation}>
+        <li onClick={() => handleClickFilter('All')}>
+          <button>All</button>
+        </li>
+        <li onClick={() => handleClickFilter('Destaques')}>
+          <button>Destaques</button>
+        </li>
+        <li onClick={() => handleClickFilter('Front-End')}>
+          <button>Front-End</button>
+        </li>
+        <li onClick={() => handleClickFilter('Back-End')} >
+          <button>Back-End</button>
+        </li>
+      </ul>
     <div className={styles.MainContainer} id="project">
       <ul>
         <li>
           <button onClick={handleClickLess}><AiFillCaretLeft/></button>
         </li>
         <li>
-          <img src={ProjectArray[project.project1].image} alt="" />
-          <h3>{ProjectArray[project.project1].title}</h3>
-          <p><strong>Descrição:</strong>{ProjectArray[project.project1].description}</p>
-          <p><strong>Tecnologias usadas:</strong> {ProjectArray[project.project1].tech}</p>
+          <img src={projectFilter[project.project1].image} alt="" />
+          <h3>{projectFilter[project.project1].title}</h3>
+          <p><strong>Descrição:</strong>{projectFilter[project.project1].description}</p>
+          <p><strong>Tecnologias usadas:</strong> {projectFilter[project.project1].tech}</p>
           <ul>
             <li>
-              <a target="_blank" href={ProjectArray[project.project1].deploy}>
+              <a target="_blank" href={projectFilter[project.project1].deploy}>
                 Site
               </a>
             </li>
             <li>
-              <a target="_blank" href={ProjectArray[project.project1].github}>
+              <a target="_blank" href={projectFilter[project.project1].github}>
                 Repositorio
               </a>
             </li>
           </ul>
         </li>
         <li>
-          <img src={ProjectArray[project.project2].image} alt="" />
-          <h3>{ProjectArray[project.project2].title}</h3>
-          <p><strong>Descrição:</strong>{ProjectArray[project.project2].description}</p>
-          <p><strong>Tecnologias usadas:</strong> {ProjectArray[project.project2].tech}</p>
+          <img src={projectFilter[project.project2].image} alt="" />
+          <h3>{projectFilter[project.project2].title}</h3>
+          <p><strong>Descrição:</strong>{projectFilter[project.project2].description}</p>
+          <p><strong>Tecnologias usadas:</strong> {projectFilter[project.project2].tech}</p>
           <ul>
             <li>
-              <a target="_blank" href={ProjectArray[project.project2].deploy}>
+              <a target="_blank" href={projectFilter[project.project2].deploy}>
                 Site
               </a>
             </li>
             <li>
-              <a target="_blank" href={ProjectArray[project.project2].github}>
+              <a target="_blank" href={projectFilter[project.project2].github}>
                 Repositorio
               </a>
             </li>
           </ul>
         </li>
         <li>
-          <img src={ProjectArray[project.project3].image} alt="" />
-          <h3>{ProjectArray[project.project3].title}</h3>
-          <p><strong>Descrição:</strong>{ProjectArray[project.project3].description}</p>
-          <p><strong>Tecnologias usadas:</strong>{ProjectArray[project.project3].tech}</p>
+          <img src={projectFilter[project.project3].image} alt="" />
+          <h3>{projectFilter[project.project3].title}</h3>
+          <p><strong>Descrição:</strong>{projectFilter[project.project3].description}</p>
+          <p><strong>Tecnologias usadas:</strong>{projectFilter[project.project3].tech}</p>
           <ul>
             <li>
-              <a target="_blank" href={ProjectArray[project.project3].deploy}>
+              <a target="_blank" href={projectFilter[project.project3].deploy}>
                 Site
               </a>
             </li>
             <li>
-              <a target="_blank" href={ProjectArray[project.project3].github}>
+              <a target="_blank" href={projectFilter[project.project3].github}>
                 Repositorio
               </a>
             </li>
@@ -142,6 +186,7 @@ export default function CardProject() {
           </button>
         </li>
       </ul>
+    </div>
     </div>
   );
 }
--- src/utils/ProjectArray.ts
+++ src/utils/ProjectArray.ts
@@ -11,7 +11,11 @@ export const ProjectArray = [
         description: ' Projeto feito na trybe, com o objetivo de consumir uma api do ibge, e mostrar as noticias do dia, um teste tecnico bastante desafiador e divertido',
         github: 'https://github.com/IanPedroBorges/TesteTecnico',
         deploy: 'https://teste-tecnico-ibge-simulator.vercel.app/',
-        tech: 'React, Typescript, Style-Modules,Context-Api, React Testing Library'
+        tech: 'React, Typescript, Style-Modules,Context-Api, React Testing Library',
+        frontend: true,
+        backend: false,
+        fullstack: false,
+        highlights: true
     },
     {
         image: recipe,
@@ -19,15 +23,23 @@ export const ProjectArray = [
         description: ' Projeto feito na trybe em grupo de 6 pessoas e focado no mobile, com o objetivo de consumir uma api de receitas,',
         github: 'https://github.com/IanPedroBorges/Recipes',
         deploy: 'https://recipes-project-tau.vercel.app/',
-        tech: 'React, Typescript, Style-Modules,Context-Api, React Testing Library'
+        tech: 'React, Typescript, Style-Modules,Context-Api, React Testing Library',
+        frontend: true,
+        backend: false,
+        fullstack: false,
+        highlights: true
     },
     {
         image: trybeWallet,
         title: 'TrybeWallet',
-        description: ' O TrybeWallet é um projeto desenvolvido na Trybe que utiliza a tecnologia Redux para criar uma plataforma que permite aos usuários acompanhar o valor das moedas com conversão. Com esta aplicação, você pode monitorar as taxas de câmbio e o valor de diferentes moedas, facilitando o acompanhamento de investimentos e transações financeiras. ',
+        description: ' O TrybeWallet é um projeto desenvolvido na Trybe que utiliza a tecnologia Redux para criar uma plataforma que permite aos usuários acompanhar o valor das moedas com conversão. ',
         github: 'https://github.com/IanPedroBorges/TrybeWallet',
         deploy: 'https://trybe-wallet-blush.vercel.app/',
-        tech: 'React, Typescript, Redux, Css, React Testing Library'
+        tech: 'React, Typescript, Redux, Css, React Testing Library',
+        frontend: true,
+        backend: false,
+        fullstack: false,
+        highlights: false
     },
     {
         image: storeManager,
@@ -35,7 +47,11 @@ export const ProjectArray = [
         description: ' O StoreManager é um projeto de backend desenvolvido na Trybe que se concentra em criar uma aplicação de gerenciamento de loja. Este projeto oferece soluções para melhorar o funcionamento de uma loja, incluindo o acompanhamento de produtos, pedidos, e clientes.',
         github: 'https://github.com/IanPedroBorges/StoreManager',
         deploy: '',
-        tech: 'Node, Javascript, Express, Mysql'
+        tech: 'Node, Javascript, Express, Mysql',
+        frontend: false,
+        backend: true,
+        fullstack: false,
+        highlights: false
     },
     {
         image: storeManager,
@@ -43,7 +59,11 @@ export const ProjectArray = [
         description: 'O BlogsApi é um projeto que se concentra em criar uma API para gerenciar blogs e postagens online. Esta API oferece uma plataforma robusta para criar, visualizar, editar e excluir blogs e suas postagens. ',
         github: 'https://github.com/IanPedroBorges/BlogsApi',
         deploy: '',
-        tech: 'Node, Javascript, Express, Mysql, Sequelize'
+        tech: 'Node, Javascript, Express, Mysql, Sequelize',
+        frontend: false,
+        backend: true,
+        fullstack: false,
+        highlights: false
     },
     {
         image: contruction,
@@ -51,6 +71,22 @@ export const ProjectArray = [
         description: ' numero 6 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s,',
         github: '',
         deploy: '',
-        tech: 'HTML, CSS , Javascript'
+        tech: '',
+        frontend: true,
+        backend: true,
+        fullstack: true,
+        highlights: true
+    },
+    {
+        image: contruction,
+        title: 'Projeto em Construção',
+        description: ' numero 6 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s,',
+        github: '',
+        deploy: '',
+        tech: '',
+        frontend: true,
+        backend: true,
+        fullstack: true,
+        highlights: true
     }
 ]
